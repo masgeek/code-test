a9de73eb495aeb49bee48caf024738e264fae34a
Munywele Sammy <barsamms@gmail.com>
2022-05-04 07:55:45 +0300

    feat: added binary search function
    added both iterative and recursive

--- src/com/munywele/BinarySearch.java
+++ src/com/munywele/BinarySearch.java
@@ -2,7 +2,58 @@ package com.munywele;
 
 public class BinarySearch {
 
-    public int elementPosition(int[] elements, int theElement){
-return -1;
+/**
+ * The function takes in an array of integers and an integer to search for. It returns the index of the
+ * integer in the array if it is found, otherwise it returns -1
+ * 
+ * @param elements the array of elements to search through
+ * @param theElement the element we are looking for
+ * @return The index of the element in the array.
+ */
+    public int iterative(int[] elements, int theElement) {
+        int low = 0;
+        int high = elements.length - 1;
+
+
+        if (high <= 0) {
+            return -1;
+        }
+
+        //use while loop loop for this one
+        while (low <= high) {
+            int mid = low + ((high - low) / 2);
+
+            if (elements[mid] == theElement) {
+                System.out.println("Mid is " + mid);
+                return mid;
+            } else if (theElement < elements[mid]) {
+                high = mid - 1;
+            } else if (theElement > elements[mid]) {
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+/**
+ * If the element is not in the array, return -1. Otherwise, find the middle of the array, and if the
+ * element is less than the middle, recursively call the function on the lower half of the array.
+ * Otherwise, recursively call the function on the upper half of the array
+ * 
+ * @param elements the array of elements to search through
+ * @param theElement the element we are looking for
+ * @param low the lowest index of the array we're searching
+ * @param high the highest index of the array
+ */
+    public int recursive(int[] elements, int theElement, int low, int high) {
+        if (low > high) {
+            return -1;
+        }
+
+        //find the flow
+        int mid = low + ((high - low) / 2);
+        
+        return -1;
     }
 }
--- tests/com/munywele/BinarySearchTest.java
+++ tests/com/munywele/BinarySearchTest.java
@@ -7,12 +7,58 @@ import static org.junit.jupiter.api.Assertions.*;
 class BinarySearchTest {
 
     @Test
-    void elementPosition() {
-        int[] elements = {1,2,4,6,8,9,12};
+    void check_position_of_6() {
+        int[] elements = {1, 2, 4, 6, 8, 9, 11, 12};
         int theElement = 6;
 
         BinarySearch binarySearch = new BinarySearch();
-        int position = binarySearch.elementPosition(elements,theElement);
+        int position = binarySearch.iterative(elements, theElement);
+
+        assertEquals(3, position);
+    }
+
+    @Test
+    void check_position_of_12() {
+        int[] elements = {0, 2, 4, 6, 8, 10, 12, 14, 16};
+        int theElement = 12;
+
+        BinarySearch binarySearch = new BinarySearch();
+        int position = binarySearch.iterative(elements, theElement);
+
+        assertEquals(6, position);
+    }
+
+    @Test
+    void check_position_of_5() {
+        int[] elements = {0, 2, 4, 6, 8, 10, 12, 14, 16};
+        int theElement = 5;
+
+        BinarySearch binarySearch = new BinarySearch();
+        int position = binarySearch.iterative(elements, theElement);
+
+        assertEquals(-1, position);
+    }
+
+    @Test
+    void check_position_of_empty() {
+        int[] elements = {};
+        int theElement = 5;
+
+        BinarySearch binarySearch = new BinarySearch();
+        int position = binarySearch.iterative(elements, theElement);
+
+        assertEquals(-1, position);
+    }
+
+    @Test
+    void check_position_of_6_recursively() {
+        int[] elements = {1, 2, 4, 6, 8, 9, 11, 12};
+        int theElement = 6;
+        int low = 0;
+        int high = elements.length - 1;
+
+        BinarySearch binarySearch = new BinarySearch();
+        int position = binarySearch.recursive(elements, theElement, low, high);
 
         assertEquals(3, position);
     }
