7b747acc93e35b1d84b0db39d1f9e640e22210dd
Akilimo Machine <dev@tsobu.co.ke>
2022-05-05 15:09:25 +0300

    feat: added summ test using hashmap for efficiency\

--- src/com/munywele/TwoSum.java
+++ src/com/munywele/TwoSum.java
@@ -1,13 +1,14 @@
 package com.munywele;
 
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
-    public int[] computeSum(int[] nums, int target, int c) {
+    public int[] computeSum(int[] nums, int target) {
 
         int len = nums.length - 1;
         for (int x = 0; x <= len; x++) {
-            for (int y = 0; y <= len; y++) {
+            for (int y = x + 1; y <= len; y++) {
                 if (nums[x] + nums[y] == target) {
                     return new int[]{x, y};
                 }
@@ -16,23 +17,41 @@ public class TwoSum {
         return new int[]{-1, -1};
     }
 
-    public int[] computeSum(int[] nums, int target) {
+    public int[] computeSumSlide(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
         int tempSum;
 
-        while (left < right) {
+        while (left < nums.length) {
             tempSum = nums[left] + nums[right];
+
+            System.out.println("Sum is " + tempSum + " for index " + left + "," + right);
             if (tempSum == target) {
                 return new int[]{left, right};
             }
 
-            if (tempSum < target) {
-                left++;
-            } else {
+            if (tempSum > target) {
                 right--;
+            } else {
+                left++;
             }
         }
         return new int[]{-1, -1};
+    }
+
+    public int[] computeSum(int[] nums, int target, int length) {
+        int[] sumIndex = new int[2];
+
+        Map<Integer, Integer> sumStore = new HashMap<>();
+        for (int x = 0; x < length; x++) {
+            int theDifference = target - nums[x];
+            if (sumStore.containsKey(theDifference)) {
+                sumIndex[0] = sumStore.get(theDifference);
+                sumIndex[1] = x;
+                break;
+            }
+            sumStore.put(nums[x], x);
+        }
+        return sumIndex;
     }
 }
--- tests/com/munywele/TwoSumTest.java
+++ tests/com/munywele/TwoSumTest.java
@@ -3,7 +3,6 @@ package com.munywele;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,12 +20,34 @@ class TwoSumTest {
     @Test
     void sum_test_1() {
 //        int[] nums = {2, 11, 15,7};
-        int[] nums = {1 , 4 , 5 , 11 , 12,8};
-        int target = 20;
+        int[] nums = {1, 4, 11, 5, 12, 8, 10};
+        int target = 19;
 
-        int[] result = twoSum.computeSum(nums, target);
+        int[] result = twoSum.computeSum(nums, target,nums.length);
 
         System.out.print(Arrays.toString(result));
-        assertArrayEquals(new int[]{4, 5}, result);
+        assertArrayEquals(new int[]{2, 5}, result);
+    }
+
+    @Test
+    void sum_test_2() {
+        int[] nums = {3, 2, 4};
+        int target = 6;
+
+        int[] result = twoSum.computeSum(nums, target,nums.length);
+
+        System.out.print(Arrays.toString(result));
+        assertArrayEquals(new int[]{1, 2}, result);
+    }
+
+    @Test
+    void sum_test_3() {
+        int[] nums = {3, 2, 4};
+        int target = 5;
+
+        int[] result = twoSum.computeSum(nums, target, nums.length);
+
+        System.out.println(Arrays.toString(result));
+        assertArrayEquals(new int[]{0, 1}, result);
     }
 }
