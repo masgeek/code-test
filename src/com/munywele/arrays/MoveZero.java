3b690fab281af736deb9734968393712f740c36f
Akilimo Machine <dev@tsobu.co.ke>
2022-05-11 12:09:42 +0300

    docs: updated method documentation

--- src/com/munywele/arrays/MoveZero.java
+++ src/com/munywele/arrays/MoveZero.java
@@ -3,6 +3,13 @@ package com.munywele.arrays;
 import java.util.*;
 
 public class MoveZero {
+    /**
+     * I'm using a map to store the zeroes and their index, then I'm using a new array to store the
+     * zeroes first, then I'm merging the old array with the new array
+     * 
+     * @param nums the array of integers
+     * @return The method is returning the new array with the zeroes moved to the left.
+     */
     public int[] moveZeroToLeft(int[] nums) {
         Map<Integer, Integer> myZeroes = new HashMap<>();
         int[] newArray = new int[nums.length];
