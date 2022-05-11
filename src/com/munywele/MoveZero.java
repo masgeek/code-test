64284cb80ccc48dd23d83cc11bc82b73a85198bf
Akilimo Machine <dev@tsobu.co.ke>
2022-05-11 12:05:08 +0300

    feat: added class for movieng zeroe
    added zero moving to the left

---
+++ src/com/munywele/MoveZero.java
@@ -0,0 +1,32 @@
+package com.munywele;
+
+import java.util.*;
+
+public class MoveZero {
+    public int[] moveZeroToLeft(int[] nums) {
+        Map<Integer, Integer> myZeroes = new HashMap<>();
+        int[] newArray = new int[nums.length];
+        for (int x = 0; x < nums.length; x++) {
+            if (nums[x] == 0) {
+                myZeroes.put(x, nums[x]);
+            }
+        }
+
+        int newIndex = 0;
+        for (Map.Entry<Integer, Integer> theZero : myZeroes.entrySet()) {
+            newArray[newIndex] = theZero.getValue();
+            newIndex++;
+        }
+
+        //next merge with the old array
+        int startIndex = myZeroes.size();
+        for (int x = 0; x < nums.length; x++) {
+            if (nums[x] != 0) {
+                newArray[startIndex] = nums[x];
+                startIndex++;
+            }
+        }
+        nums = newArray;
+        return nums;
+    }
+}
---
+++ tests/com/munywele/MoveZeroTest.java
@@ -0,0 +1,34 @@
+package com.munywele;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoveZeroTest {
+
+    MoveZero moveZero;
+
+    @BeforeEach
+    void setUp() {
+        moveZero = new MoveZero();
+    }
+
+    @Test
+    void move_zeroes_to_left() {
+        int[] nums = {1, 10, 20, 0, 59, 63, 0, 88, 0};
+
+        int[] result = moveZero.moveZeroToLeft(nums);
+
+        assertArrayEquals(new int[]{0, 0, 0, 1, 10, 20, 59, 63, 88}, result);
+    }
+
+    @Test
+    void move_zeroes_to_left_2() {
+        int[] nums = {1, 10, 20, 0, 59, 63, 0, 88, 0};
+
+        int[] result = moveZero.moveZeroToLeft(nums);
+
+        assertArrayEquals(new int[]{0, 0, 0, 1, 10, 20, 59, 63, 88}, result);
+    }
+}
