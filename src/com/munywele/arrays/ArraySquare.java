ea3c63725ab62e8c34b7f1c1c4ce7e6e9cd5230a
Akilimo Machine <dev@tsobu.co.ke>
2022-05-13 09:08:43 +0300

    refactor: added sorting using code and not inbuilt option

--- src/com/munywele/arrays/ArraySquare.java
+++ src/com/munywele/arrays/ArraySquare.java
@@ -1,7 +1,5 @@
 package com.munywele.arrays;
 
-import java.util.Arrays;
-
 /**
  * Given an array of integers, return an array of the squares of each integer, sorted in ascending
  * order
@@ -17,14 +15,34 @@ public class ArraySquare {
     public int[] computeSquare(int[] nums) {
         int[] squaredArr = new int[nums.length];
 
-        for (int x = 0; x < nums.length; x++) {
-            int squared = nums[x] * nums[x];
-            squaredArr[x] = squared;
+        int left = 0;
+        int right = nums.length - 1;
+
+        for (int x = nums.length - 1; x >= 0; x--) {
+            int square;
+
+            //get values and make them absolute
+            int leftTemp = nums[left];
+            int rightTemp = nums[right];
+            if (leftTemp < 0) {
+                leftTemp = (leftTemp) * (-1);
+            }
+            if (rightTemp < 0) {
+                rightTemp = (rightTemp) * (-1);
+            }
+            if (leftTemp < rightTemp) {
+                square = nums[right];
+                right--;
+            } else {
+                square = nums[left];
+                left++;
+            }
+            squaredArr[x] = square * square;
         }
 
         //sort the array
         //@TODO develop merge sort algorithm later
-        Arrays.sort(squaredArr); //built in
+        // Arrays.sort(squaredArr); //built in
 
 
         return squaredArr;
