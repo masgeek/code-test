28180dd0f9bc6426fc3d808658586a56a8b01245
Akilimo Machine <dev@tsobu.co.ke>
2022-05-13 08:43:30 +0300

    fix: added built in sorting

--- src/com/munywele/arrays/ArraySquare.java
+++ src/com/munywele/arrays/ArraySquare.java
@@ -1,6 +1,19 @@
 package com.munywele.arrays;
 
+import java.util.Arrays;
+
+/**
+ * Given an array of integers, return an array of the squares of each integer, sorted in ascending
+ * order
+ */
 public class ArraySquare {
+    /**
+     * Given an array of integers, return an array of the squares of each integer, sorted in ascending
+     * order
+     *
+     * @param nums an array of integers
+     * @return The squared array
+     */
     public int[] computeSquare(int[] nums) {
         int[] squaredArr = new int[nums.length];
 
@@ -8,6 +21,12 @@ public class ArraySquare {
             int squared = nums[x] * nums[x];
             squaredArr[x] = squared;
         }
+
+        //sort the array
+        //@TODO develop merge sort algorithm later
+        Arrays.sort(squaredArr); //built in
+
+
         return squaredArr;
     }
 }
--- tests/com/munywele/arrays/ArraySquareTest.java
+++ tests/com/munywele/arrays/ArraySquareTest.java
@@ -21,7 +21,30 @@ class ArraySquareTest {
         int[] nums = {-4, -1, 0, 3, 10};
         int[] result = arraySquare.computeSquare(nums);
         int[] expected = new int[]{0, 1, 9, 16, 100};
+        assertEquals(Arrays.toString(expected), Arrays.toString(result));
+    }
 
+    @Test
+    void return_array_of_squares_b() {
+        int[] nums = {-7, -3, 2, 3, 11};
+        int[] result = arraySquare.computeSquare(nums);
+        int[] expected = new int[]{4, 9, 9, 49, 121};
+        assertEquals(Arrays.toString(expected), Arrays.toString(result));
+    }
+
+    @Test
+    void return_array_of_squares_c() {
+        int[] nums = {-100,100};
+        int[] result = arraySquare.computeSquare(nums);
+        int[] expected = new int[]{10000,10000};
+        assertEquals(Arrays.toString(expected), Arrays.toString(result));
+    }
+
+    @Test
+    void return_array_of_squares_d() {
+        int[] nums = {-5};
+        int[] result = arraySquare.computeSquare(nums);
+        int[] expected = new int[]{25};
         assertEquals(Arrays.toString(expected), Arrays.toString(result));
     }
 }
