0145c6b950a0c9cf137f84621a9eced64432ea8e
Akilimo Machine <dev@tsobu.co.ke>
2022-05-17 13:04:50 +0300

    refactor: removed redundant files

---
+++ src/com/munywele/maths/MissingNumber.java
@@ -0,0 +1,10 @@
+package com.munywele.maths;
+
+import java.util.List;
+
+public class MissingNumber {
+    public int findMissing(List<Integer> input) {
+
+        return -1;
+    }
+}
---
+++ tests/com/munywele/maths/MissingNumberTest.java
@@ -0,0 +1,31 @@
+package com.munywele.maths;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.*;
+class MissingNumberTest {
+
+    MissingNumber missingNumber;
+    @BeforeEach
+    void setUp() {
+        missingNumber = new MissingNumber();
+    }
+
+    @Test
+    void find_missing_number() {
+        List<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(3);
+        numbers.add(7);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(0);
+        numbers.add(4);
+        numbers.add(5);
+        int result = missingNumber.findMissing(numbers);
+
+        assertEquals(6,result);
+    }
+}
