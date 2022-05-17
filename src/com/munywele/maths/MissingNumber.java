05d1aaae6c7e651a32d377fadf30232de9bfd06f
Akilimo Machine <dev@tsobu.co.ke>
2022-05-17 13:57:19 +0300

    feat: added missing number check algorithm

--- src/com/munywele/maths/MissingNumber.java
+++ src/com/munywele/maths/MissingNumber.java
@@ -3,8 +3,43 @@ package com.munywele.maths;
 import java.util.List;
 
 public class MissingNumber {
+    /**
+     * It iterates through the list, and if the list doesn't contain the current number, it returns the
+     * current number
+     * Time complexity is O(n^2)
+     *
+     * @param input a list of integers
+     * @return The number that is missing from the list.
+     */
     public int findMissing(List<Integer> input) {
 
-        return -1;
+        int numberCounter = 0;
+        for (int number : input) {
+            if (!input.contains(numberCounter)) {
+                break;
+            }
+            numberCounter++;
+        }
+
+        return numberCounter;
+    }
+
+    /**
+     * Given a list of integers, find the missing integer
+     * <p>
+     * Use formula (n*(n+1))/2
+     * Time complexity is O(n)
+     *
+     * @param input a list of integers
+     */
+    public int findMissingEfficiently(List<Integer> input) {
+        int numberSum = 0;
+        int n = input.size();
+        for (int number : input) {
+            numberSum = numberSum + number;
+        }
+
+        int sumOfElements = (n*(n + 1)) / 2;
+        return sumOfElements - numberSum;
     }
 }
--- tests/com/munywele/maths/MissingNumberTest.java
+++ tests/com/munywele/maths/MissingNumberTest.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.*;
+
 class MissingNumberTest {
 
     MissingNumber missingNumber;
+
     @BeforeEach
     void setUp() {
         missingNumber = new MissingNumber();
@@ -26,6 +28,39 @@ class MissingNumberTest {
         numbers.add(5);
         int result = missingNumber.findMissing(numbers);
 
-        assertEquals(6,result);
+        assertEquals(6, result);
+    }
+
+    @Test
+    void find_missing_number_a() {
+        List<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(0);
+        int result = missingNumber.findMissing(numbers);
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    void find_missing_number_c() {
+        List<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(3);
+        numbers.add(7);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(0);
+        numbers.add(4);
+        numbers.add(5);
+        int result = missingNumber.findMissingEfficiently(numbers);
+
+        assertEquals(6, result);
+    }
+
+    @Test
+    void find_missing_number_d() {
+        List<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(0);
+        int result = missingNumber.findMissingEfficiently(numbers);
+
+        assertEquals(1, result);
     }
 }
