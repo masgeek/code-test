ce6f5806cba0e72c96bf8289144133077b120e34
Akilimo Machine <dev@tsobu.co.ke>
2022-05-19 14:21:23 +0300

    docs: updated method documentation

--- src/com/munywele/strings/AddTwoString.java
+++ src/com/munywele/strings/AddTwoString.java
@@ -1,6 +1,14 @@
 package com.munywele.strings;
 
 public class AddTwoString {
+    /**
+     * It converts the two strings to numbers, adds them together, and then converts the result back to
+     * a string
+     * 
+     * @param num1 "123"
+     * @param num2 "9"
+     * @return The sum of the two numbers.
+     */
     public String addStrings(String num1, String num2) {
 
         int number = convertToNumber(num1);
@@ -9,11 +17,23 @@ public class AddTwoString {
         return String.valueOf(total);
     }
 
+    /**
+     * It converts a number to a string.
+     * 
+     * @param num The number to convert to a string.
+     * @return The method convertToString is being returned.
+     */
     public String convertNumber(int num) {
         return convertToString(num);
     }
 
 
+    /**
+     * It takes a string of numbers and converts it to an integer
+     * 
+     * @param numStr The string that you want to convert to a number.
+     * @return Integer equivalet of the string value
+     */
     private int convertToNumber(String numStr) {
         char[] nums = numStr.toCharArray();
         int num = 0;
@@ -25,6 +45,12 @@ public class AddTwoString {
         return num;
     }
 
+    /**
+     * It takes an integer and returns a string representation of that integer
+     * 
+     * @param num The number to be converted to a string
+     * @return The number euivalent in string format
+     */
     private String convertToString(int num) {
         int rem = num;
         int modulusVal = num;
