4a78cf9ba85c1bd076b55530fdd5b65a6ad60ebf
Akilimo Machine <dev@tsobu.co.ke>
2022-05-20 12:22:13 +0300

    feat: binary trees
    created binary tree node

---
+++ src/com/munywele/binarytree/Node.java
@@ -0,0 +1,13 @@
+package com.munywele.binarytree;
+
+public class Node {
+    int key;
+    Node right;
+    Node left;
+
+    public Node(int key){
+        this.key = key;
+        this.right = null;
+        this.left = null;
+    }
+}
